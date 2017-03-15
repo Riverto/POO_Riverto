abff236be8e0b6991a6661665b14569f07f07d3d
riverto <a07025304@itesm.mx>
2017-03-15 12:37:27 -0600

    Ex: 5

---
+++ InterfacesExercises/src/interfacesexercises/HandyPerson.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacesexercises;
+
+/**
+ *
+ * @author Riverto
+ */
+public class HandyPerson extends Human implements Plumber,Electrician   {
+    public HandyPerson(String name) {
+        super(name);
+    }
+
+    @Override
+    public String unplugDrain() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String changeBulb() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ InterfacesExercises/src/interfacesexercises/IndustrialPlumber.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacesexercises;
+
+/**
+ *
+ * @author Riverto
+ */
+public class IndustrialPlumber extends Human implements Plumber,Electrician{
+
+    public IndustrialPlumber(String name) {
+        super(name);
+    }
+
+    @Override
+    public String unplugDrain() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String changeBulb() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
