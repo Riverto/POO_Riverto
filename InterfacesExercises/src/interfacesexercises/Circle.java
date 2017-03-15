31778157b195222716b4429038f15270414bd021
riverto <a07025304@itesm.mx>
2017-03-15 12:02:41 -0600

    Ex: 3

---
+++ InterfacesExercises/src/interfacesexercises/Circle.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacesexercises;
+
+/**
+ *
+ * @author Riverto
+ */
+public class Circle implements GeometricObject{
+    protected double radius = 1.0;
+
+    public Circle(double radius) {
+        this.radius=radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2*Math.PI*radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI*Math.pow(radius, 2);
+    }
+    
+    
+}
